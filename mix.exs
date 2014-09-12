ae17b5661893528bfc9a6d374146e31dca7823c4
Paul Wilson <paulanthonywilson@gmail.com>
2014-09-12 14:50:45 +0100

    upgrade to elixir-1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Stripe2qif.Mixfile do
   def project do
     [ app: :stripe2qif,
       version: "0.3.0",
-      elixir: "1.0.0-rc1",
+      elixir: "1.0.0",
       deps: deps,
       escript: escript]
   end
